52b9c19a94f59a1b4f7f73e01b75054c45ae35f2
JuliaCaesar <101206316+JuliaCaesar@users.noreply.github.com>
2025-03-28 23:06:57 -0500

    too tired to continue fix later

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 
-import msvcrt
+import keyboard
 
-print("Press any key to continue...")
-msvcrt.getch()
-print("Continuing...")
+print("Press 1 for more information or any other key to continue...")
+
+while True:
+    if keyboard.is_pressed('1'):
+        print("Showing more information...")
+        break
+    elif keyboard.is_pressed():
+        print("Continuing...")
+        break
